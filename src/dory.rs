8655ced004471db78bad58825859033192ae313a
Pupil1999 <bfangong@gmail.com>
2023-04-16 01:59:04 -0400

    modified:   .gitignore 	modified:   src/dory.rs 	modified:   src/main.rs

--- src/dory.rs
+++ src/dory.rs
@@ -1,8 +1,16 @@
+use crate::ConstraintSystem;
+use ark_ec::pairing::{Pairing};
+use ark_bls12_381::{Bls12_381};
 pub struct DoryProof{
     
 }
 
 pub struct DoryPrecomputation {
     _m: u64,
-    
+}
+
+impl DoryPrecomputation {
+    pub fn generate_from_qap(&mut self, QAP: &ConstraintSystem) {
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,19 @@ use ark_bls12_381::{Bls12_381, G1Affine, G2Affine, G1Projective as G1, G2Project
 
 #[allow(non_snake_case)]
 fn main() {
+
+    let G = G1::generator();
+    let H = G2::generator();
+
+    let s_13 = Fr::from(13);
+    let s_12 = Fr::from(12);
+    let s_156 = Fr::from(156);
+
+    let G_13 = G * s_13;
+    let H_12 = H * s_12;
+    let G_156 = G * s_156;
     
+    let gt_156 = Bls12_381::pairing(G_13, H_12);
+    let gt_156_2 = gt_156.mul(s_13);
+    assert_eq!(gt_156_2, Bls12_381::pairing(G_156, H * s_13));
 }
