251b910ab558c360f97bf5a3c0c8834c45de52f1
Pupil1999 <bfangong@gmail.com>
2023-04-16 03:04:36 -0400

    modified:   src/dory.rs

--- src/dory.rs
+++ src/dory.rs
@@ -1,16 +1,19 @@
 use crate::ConstraintSystem;
-use ark_ec::pairing::{Pairing};
-use ark_bls12_381::{Bls12_381};
+use ark_ec::{pairing::{Pairing, PairingOutput}, bls12::Bls12};
+use ark_bls12_381::{Bls12_381, Config};
+
+type Gt = PairingOutput<Bls12<Config>>;
+
 pub struct DoryProof{
     
 }
 
 pub struct DoryPrecomputation {
     _m: u64,
+    pub precoms: Vec<(Gt, Gt, Gt, Gt)>
 }
-
 impl DoryPrecomputation {
     pub fn generate_from_qap(&mut self, QAP: &ConstraintSystem) {
-
+        let (_, n, _) = QAP.get_size();
     }
 }
